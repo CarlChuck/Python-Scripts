2e8ba47ce27a090022698353aa07cb80b0724a87
Carl Chuck <carl_chuck@hotmail.co.uk>
2018-05-13 17:24:23 +0100

    updated

--- twitterFollowerTemplate.py
+++ twitterFollowerTemplate.py
@@ -14,41 +14,32 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-#defines the followers and friends lists
+# defines the followers and friends lists
 followers = api.followers_ids(screen_name)
 friends = api.friends_ids(screen_name)
 
 
-#this loop removes all who are not following back
-for f in friends:
-    if f not in followers:
-        api.destroy_friendship(f)
-        nsecs=random.randint(5,25)
-        time.sleep(nsecs)
-    else:
-        continue
+# this function removes all who are not following back
+def remove_non_followers():
+    for f in friends:
+        if f not in followers:
+            api.destroy_friendship(f)
+            nsecs=random.randint(5, 25)
+            time.sleep(nsecs)
+        else:
+            continue
+
 
 # This loop follows back those that have followed you.
-for follower in tweepy.Cursor(api.followers).items():
-    follower.follow()
+def follow_followers():
+    for follower in tweepy.Cursor(api.followers).items():
+        follower.follow()
+
 
 # Swap out the topics to reflect actual topics of interest
-for status in tweepy.Cursor(api.search, q='#topic').items(25):
-    api.create_friendship(id=status.user.id)
-    nsecs=random.randint(5,25)
-    time.sleep(nsecs)
+def follow_by_topic(topic):
+    for status in tweepy.Cursor(api.search, q=topic).items(25):
+        api.create_friendship(id=status.user.id)
+        nsecs=random.randint(5, 25)
+        time.sleep(nsecs)
 
-for status in tweepy.Cursor(api.search, q='#topic').items(25):
-    api.create_friendship(id=status.user.id)
-    nsecs=random.randint(5,25)
-    time.sleep(nsecs)
-
-for status in tweepy.Cursor(api.search, q='#topic').items(25):
-    api.create_friendship(id=status.user.id)
-    nsecs=random.randint(5,25)
-    time.sleep(nsecs)
-
-for status in tweepy.Cursor(api.search, q='#topic').items(25):
-    api.create_friendship(id=status.user.id)
-    nsecs=random.randint(5,25)
-    time.sleep(nsecs)
